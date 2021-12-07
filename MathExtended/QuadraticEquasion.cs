2efa4914c96e483760c5d1d41eef2ca8c871db85
dL <patdud02@wp.pl>
2021-12-07 14:21:15 +0100

    uodate

--- MathExtended/QuadraticEquasion.cs
+++ MathExtended/QuadraticEquasion.cs
@@ -3,9 +3,10 @@ namespace MathExtended
 {
     public class QuadraticEquasion
     {
-        double A, B;
+        double A, B, rotationX, rotationY;
         Vector vector;
         Interval interval;
+
         public QuadraticEquasion(double A, double B, double C)
         {
             this.A = A;
@@ -21,11 +22,21 @@ namespace MathExtended
             this.B = B;
             this.interval = interval;
         }
+        public void Rotate(Rotation rotationOX, Rotation rotationOY)
+        {
+            if (rotationOX == Rotation.OX) rotationX = -1.0;
+            if (rotationOX == Rotation.NOX) rotationX = 1.0;
+
+            if (rotationOY == Rotation.OY) rotationY = -1.0;
+            if (rotationOY == Rotation.NOY) rotationY = 1.0;
+        }
 
         public Point CalculateFuntion(double argument)
         {
+            argument -= vector.GetX();
+            argument *= rotationX;
             double Y = (A * argument * argument) + (B * argument) + vector.GetY();
-            return new Point(argument,Y,vector.GetZ());
+            return new Point(argument,Y* rotationY,vector.GetZ());
         }
     }
 }
